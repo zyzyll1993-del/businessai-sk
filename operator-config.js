fb6a8b11c6176525cc303a0cfa48f4535f79824a
zyzyll1993-del <zyzyll1993@gmail.com>
2026-09-04 10:26:10 +0200

    Add centralized public operator configuration

---
+++ operator-config.js
@@ -0,0 +1,25 @@
+(()=>{
+ const data={
+  legalName:'',
+  legalForm:'',
+  ico:'',
+  address:'',
+  email:'',
+  country:'Slovakia'
+ };
+ const clean=v=>String(v??'').trim();
+ const esc=v=>clean(v).replace(/[&<>"']/g,ch=>({'&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot;',"'":'&#39;'}[ch]));
+ const complete=()=>Boolean(clean(data.legalName)&&clean(data.address)&&clean(data.email));
+ function lines(){
+  const out=[];
+  if(clean(data.legalName))out.push(clean(data.legalName));
+  if(clean(data.legalForm))out.push(clean(data.legalForm));
+  if(clean(data.ico))out.push(`IČO: ${clean(data.ico)}`);
+  if(clean(data.address))out.push(clean(data.address));
+  if(clean(data.country))out.push(clean(data.country));
+  if(clean(data.email))out.push(clean(data.email));
+  return out;
+ }
+ function html(){return lines().map(esc).join('<br>')}
+ window.BusinessAIOperator={...data,complete,lines,html};
+})();
